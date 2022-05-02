8fd568781fbd673543533d3a716d03a4ec3c36f2
Max Fan <root@max.fan>
2022-05-02 14:30:33 -0500

    Add caching when building knn model in flask app

--- analytics/server.py
+++ analytics/server.py
@@ -2,9 +2,11 @@
 from flask import Flask
 import deltas
 import analyze
+import pandas as pd
+import pickle
+import os
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def root():
@@ -13,9 +15,23 @@ def root():
 
 @app.route('/trip/<tripID>/stop/<stopID>')
 def timeToBusAtStop(tripID, stopID):
-    diffs = deltas.generate_deltas(analyze.generate_df())
     try:
         delta = diffs.deltas[tripID][stopID]
         return delta
     except:
         return "Invalid trip ID or stop ID", 400
+
+def generate_knn():
+    if os.path.isfile("index.pickle"):
+        with open('index.pickle') as f:
+            diffs = pickle.load(f)
+
+    else:
+        df = pd.read_csv("scrape/responses.csv")
+        diffs = deltas.generate_deltas(df)
+        with open('index.pickle','w') as f:
+            pickle.dump(diffs, f)
+
+    return diffs
+
+diffs = generate_knn()
