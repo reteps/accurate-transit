fdb0b891ed856a9ff649eb963ec6fa4fd0f2bc22
Max Fan <root@max.fan>
2022-05-02 15:12:12 -0500

    Fix Timedelta conversion to int/seconds type error

--- analytics/server.py
+++ analytics/server.py
@@ -3,6 +3,7 @@ from flask import Flask
 import deltas
 import analyze
 import pandas as pd
+import numpy as np
 import pickle
 import os
 
@@ -17,8 +18,8 @@ def timeToBusAtStop(tripID, stopID):
     try:
         deltas = diffs.deltas[tripID][stopID]
         time_deltas = [x for _, x in deltas]
-        return str(int(pd.Series(time_deltas).median()))
-    except:
+        return str(int(pd.Series(time_deltas).median() / np.timedelta64(1, 's')))
+    except KeyError:
         return "Invalid trip ID or stop ID", 400
 
 def generate_knn():
