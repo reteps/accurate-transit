f9b7ee0b8c57f69d2cc66bf11b8f530b36cea271
Max Fan <root@max.fan>
2022-05-02 14:57:53 -0500

    Fix server pickling

--- analytics/server.py
+++ analytics/server.py
@@ -12,27 +12,26 @@ app = Flask(__name__)
 def root():
     return 'Analytics service running!'
 
-
 @app.route('/trip/<tripID>/stop/<stopID>')
 def timeToBusAtStop(tripID, stopID):
     try:
         deltas = diffs.deltas[tripID][stopID]
         time_deltas = [x for _, x in deltas]
-        return pd.Series(time_deltas).median()
+        return str(pd.Series(time_deltas).median())
     except:
         return "Invalid trip ID or stop ID", 400
 
 def generate_knn():
     if os.path.isfile("index.pickle"):
-        with open('index.pickle') as f:
+        with open('index.pickle', 'rb') as f:
             diffs = pickle.load(f)
 
     else:
         df = pd.read_csv("scrape/responses.csv")
         diffs = deltas.generate_deltas(df)
-        with open('index.pickle','w') as f:
+        with open('index.pickle','wb') as f:
             pickle.dump(diffs, f)
 
     return diffs
-
 diffs = generate_knn()
+
